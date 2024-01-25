6f8bb0f01b8f04d906794f05aa8fee5f2fb9aefb
vichhaichhorm <155121433+vichhaichhorm@users.noreply.github.com>
2024-01-25 20:07:21 +0700

    verson2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+   class person{
+      private String name;
+      private String address;
+      person(String name, String address){
+         this.address=address;
+         this.name=name;
+      }
+
+   }
+   class student extends person{
+      private String course;
+      private float score;
+      int numberofcourse;
+      student(String name,String addreess,int numberofcourse){
+         super(name,addreess);
+         this.numberofcourse=numberofcourse;
+
+      }
+      public void addcourse(String course, float score){
+         this.course=course;
+         this.score=score;
+      }
+   }
+   public static void main(String[] args) {
+
+   }
 }
